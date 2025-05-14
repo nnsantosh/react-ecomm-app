7b3f1c43fc83e44877306773eeea5f70309e0ab1
snijalingappa <santosh.nijalingappa@cj.com>
2025-05-14 21:34:35 +0530

    Fix case sensitivity issues and add build debugging

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import CartPage from "./pages/CartPage.jsx";
 import CheckoutPage from "./pages/CheckoutPage.jsx";
-import ThankYouPage from './pages/ThankYouPage.jsx';
+//import ThankYouPage from './pages/ThankYouPage.jsx';
 
 function App() {
     return (
@@ -13,7 +13,7 @@ function App() {
                     <Route path="/" element={<HomePage />} />
                     <Route path="/cart" element={<CartPage />} />
                     <Route path="/checkout" element={<CheckoutPage />} />
-                    <Route path="/thank-you" element={<ThankYouPage />} />
+                   {/*  <Route path="/thank-you" element={<ThankYouPage />} /> */}
                 </Routes>
             </Router>
         </CartProvider>
